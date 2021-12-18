913d2e5985a1ade636a7f9adbb3ca6fac3e3132e
Alamgir <f180149@cfd.nu.edu.pk>
2021-12-18 13:37:42 +0500

    data updated

--- Part(2).R
+++ Part(2).R
@@ -4,30 +4,6 @@ library(gtrendsR)
 my_data<-gtrends(keyword = 'Machine Learning',geo='',time='today+5-y',gprop = c("web", "news", "images", "froogle", "youtube"))
 #Extracting dataset on basics of interest over time
 yearly_data<-data.frame(my_data$interest_over_time)
-#Creating data of dates on basic of their years
-dates<-format(yearly_data$date,format = "%Y")
-#Removing dulplcates from years
-dates<-dates[!duplicated(dates)]
-#Creating a vector
-hits_counts <- c(rep(0, length(dates)))
-#This loop will extract dates on basics of their year
-#After that it will increment the value of hits of that year
-for(i in 1:nrow(yearly_data))
-{
+#Creating graph on basics of dates and hits
 
-  date1<-format(yearly_data[i,1],format = "%Y")
-
-  for(j in 1:length(dates))
-  {
-    if(date1==dates[j])
-    {
-      
-      hits_counts[j]=hits_counts[j]+1
-      break
-    }
-  }
-
-  
-}
-#Plotting graph
-plot(dates, hits_counts, type = "l",ylim=c(0,100),xlab='Years',ylab='Hit counts',main="Hits with respect to years of 'Machine Learning'")
+ggplot(yearly_data,aes(x=date,y=hits))+geom_line(stat="identity",color="blue")+labs(title="Machine Learning Searches on Google",x="Years",y="Total Counts",color="Legend Title\n")+theme_minimal()
